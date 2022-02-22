ecba4e137fc27c322911d8d3d1e08a5b26c0d6ef
James Pacheco <jamesrp13@gmail.com>
2022-02-22 15:26:09 -0700

    Add read functions

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,44 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
+import { AccountInfo, clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
 import Dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 Dotenv.config();
 
-async function sendSol(origin: Keypair, recipient: PublicKey, amount: number) {
-    // Create a new transaction
-    const transaction = new Transaction();
-
-    // Add SystemProgram.transfer instructions to transfer SOL
-    transaction.add(
-        SystemProgram.transfer({
-            fromPubkey: origin.publicKey,
-            toPubkey: recipient,
-            lamports: LAMPORTS_PER_SOL * amount
+async function getBalanceUsingJSONRPC(address: PublicKey): Promise<number> {
+    const url = clusterApiUrl('devnet')
+    console.log(url);
+    return fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "getBalance",
+            "params": [
+                address.toBase58()
+            ]
         })
-    )
+    }).then(response => response.json())
+    .then(json => {
+        if (json.error) {
+            throw json.error
+        }
 
-    const connection = new Connection(clusterApiUrl('devnet'));
-
-    // Actually send the transaction
-    return sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [origin]
-    ).then((signature) => {
-        // When a transaction succeeds it returns the transaction signature
-        // You can view all transactions with the Solana Explorer
-        console.log(`TRANSACTION SUCCEEDED! Go to https://explorer.solana.com/tx/${signature}?cluster=devnet to see the transaction details.`);
-    }).catch((error) => {
-        // Your transaction will probably fail if this is your first time since you have no SOL
-        // You can get test SOL for devnet at https://solanatools.xyz/faucet/devnet.html
-        // You'll need to provide your base58 address, which you can get by logging 
-        // `ownerKeypair.publicKey.toBase58()` to the console
-        console.log('TRANSACTION FAILED:', error);
+        return json['result']['value'] as number;
     })
+    .catch(error => {
+        throw error
+    })
+}
+
+async function getBalanceUsingWeb3(address: PublicKey): Promise<number> {
+    const connection = new Connection(clusterApiUrl('devnet'));
+    return connection.getBalance(address);
+}
+
+async function getAccountInfo(address: PublicKey): Promise<AccountInfo<Buffer> | null> {
+    const connection = new Connection(clusterApiUrl('devnet'));
+    return connection.getAccountInfo(address);
 }
 
 // If you don't already have a private key in your env file, you can add an existing one 
@@ -45,8 +50,18 @@ const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
 const secretKey = Uint8Array.from(secret)
 const owner = Keypair.fromSecretKey(secretKey)
 
-// We're going to send some SOL to somebody (on devnet so it doesn't have any real value)
-// You can find a random public address to put here, use this one, or generate a new one
-const repicient = new PublicKey('BpnBxp5KvnupqYVutjYwyhmQi7wQrU5xZXXGRgZcKDSj')
+getBalanceUsingJSONRPC(owner.publicKey).then(balance => {
+    console.log(`The balance for address ${owner.publicKey} is ${JSON.stringify(balance)}`);
+}).catch(error => {
+    console.log(`Error getting balance for address ${owner.publicKey}. Error: ${JSON.stringify(error)}`)
+})
 
-sendSol(owner, repicient, 0.1);
+getBalanceUsingWeb3(owner.publicKey).then(balance => {
+    console.log(`The balance for address ${owner.publicKey} is ${JSON.stringify(balance)}`);
+}).catch(error => {
+    console.log(`Error getting balance for address ${owner.publicKey}. Error: ${JSON.stringify(error)}`)
+})
+
+getAccountInfo(owner.publicKey).then(info => {
+    console.log(info);
+})
