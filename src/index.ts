fac94b6184fb64f49e2e018f331a6e67e31d78c6
James Pacheco <jamesrp13@gmail.com>
2022-02-22 14:52:35 -0700

    Clean up "writing" portion

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, TransferParams } from '@solana/web3.js';
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
 import Dotenv from 'dotenv';
 
 Dotenv.config();
+
+async function sendSol(origin: Keypair, recipient: PublicKey, amount: number) {
+    // Create a new transaction
+    const transaction = new Transaction();
+
+    // Add SystemProgram.transfer instructions to transfer SOL
+    transaction.add(
+        SystemProgram.transfer({
+            fromPubkey: origin.publicKey,
+            toPubkey: recipient,
+            lamports: LAMPORTS_PER_SOL * amount
+        })
+    )
+
+    const connection = new Connection(clusterApiUrl('devnet'));
+
+    // Actually send the transaction
+    return sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [origin]
+    ).then((signature) => {
+        // When a transaction succeeds it returns the transaction signature
+        // You can view all transactions with the Solana Explorer
+        console.log(`TRANSACTION SUCCEEDED! Go to https://explorer.solana.com/tx/${signature}?cluster=devnet to see the transaction details.`);
+    }).catch((error) => {
+        // Your transaction will probably fail if this is your first time since you have no SOL
+        // You can get test SOL for devnet at https://solanatools.xyz/faucet/devnet.html
+        // You'll need to provide your base58 address, which you can get by logging 
+        // `ownerKeypair.publicKey.toBase58()` to the console
+        console.log('TRANSACTION FAILED:', error);
+    })
+}
 
 // If you don't already have a private key in your env file, you can add an existing one 
 // or generate a new one using `const ownerKeypair = Keypair.generate()`
@@ -9,35 +42,11 @@ Dotenv.config();
 // You can copy this array from the console and paste it into your `.env` file as a string
 // that you can then parse into an array
 const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
-
 const secretKey = Uint8Array.from(secret)
-const ownerKeypair = Keypair.fromSecretKey(secretKey)
-console.log(ownerKeypair.secretKey);
-const repicientPubkey = new PublicKey('BpnBxp5KvnupqYVutjYwyhmQi7wQrU5xZXXGRgZcKDSj')
-const transaction = new Transaction();
+const owner = Keypair.fromSecretKey(secretKey)
 
-transaction.add(
-    SystemProgram.transfer({
-        fromPubkey: ownerKeypair.publicKey,
-        toPubkey: repicientPubkey,
-        lamports: LAMPORTS_PER_SOL * 0.1
-    })
-)
+// We're going to send some SOL to somebody (on devnet so it doesn't have any real value)
+// You can find a random public address to put here, use this one, or generate a new one
+const repicient = new PublicKey('BpnBxp5KvnupqYVutjYwyhmQi7wQrU5xZXXGRgZcKDSj')
 
-const connection = new Connection(clusterApiUrl('devnet'));
-
-sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [ownerKeypair]
-).then((signature) => {
-    // When a transaction succeeds it returns the transaction signature
-    // You can view all transactions with the Solana Explorer
-    console.log(`TRANSACTION SUCCEEDED! Go to https://explorer.solana.com/tx/${signature}?cluster=devnet to see the transaction details.`);
-}).catch((error) => {
-    // Your transaction will probably fail if this is your first time since you have no SOL
-    // You can get test SOL for devnet at https://solanatools.xyz/faucet/devnet.html
-    // You'll need to provide your base58 address, which you can get by logging 
-    // `ownerKeypair.publicKey.toBase58()` to the console
-    console.log('TRANSACTION FAILED:', error);
-})
+sendSol(owner, repicient, 0.1);
